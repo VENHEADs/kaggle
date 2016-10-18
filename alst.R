ec0ef7903ede2e5e03e25c910513bbbc8bc7e428
VENHEADs <VENHEADs@yandex.ru>
2016-10-18 08:46:36 +0300

    Update alst.R

--- alst.R
+++ alst.R
@@ -13,7 +13,7 @@ dtest = xgb.DMatrix(testm)
 
 
 
-xgb_params = list(
+gb_params = list(
     colsample_bytree = 0.7,
     subsample = 0.7,
     eta = 0.05,
@@ -21,9 +21,11 @@ xgb_params = list(
     max_depth = 8,
     num_parallel_tree = 10,
     min_child_weight = 3,
-    base_score = 8)
+    base_score =8)
 
 gbdt = xgb.train(xgb_params, dtrain, nrounds=900,nfold=6)
 pre = exp(predict(gbdt,dtest))
 testx[,2]<-pre
-write.csv(testx,"base8_nr900_p10.csv",row.names = FALSE)
+write.csv(testx,"base8_nr900_p10¬_cleaned.csv",row.names = FALSE)	
+
+
